f2881e1f982f691d7cc79019e974d012c4e84f8d
ZeroCho <zerohch0@gmail.com>
2020-03-20 00:10:18 +0900

    Initial Commit

---
+++ js/front/next.config.js
@@ -0,0 +1,25 @@
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+});
+const webpack = require('webpack');
+const CompressionPlugin = require('compression-webpack-plugin');
+
+module.exports = withBundleAnalyzer({
+  distDir: '.next',
+  webpack(config) {
+    const prod = process.env.NODE_ENV === 'production';
+    const plugins = [
+      ...config.plugins,
+      new webpack.ContextReplacementPlugin(/moment[/\\]locale$/, /^\.\/ko$/),
+    ];
+    if (prod) {
+      plugins.push(new CompressionPlugin()); // main.js.gz
+    }
+    return {
+      ...config,
+      mode: prod ? 'production' : 'development',
+      devtool: prod ? 'hidden-source-map' : 'eval',
+      plugins,
+    };
+  },
+});
