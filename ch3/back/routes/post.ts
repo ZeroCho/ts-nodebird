ec98744f0bf274b5264b0c870a1436f16b53aae4
cptkuk91 <cptkuk91@gmail.com>
2022-09-13 16:03:22 +0900

    [수정] routes/post.ts multer s3

--- ch3/back/routes/post.ts
+++ ch3/back/routes/post.ts
@@ -1,29 +1,36 @@
-import AWS from 'aws-sdk';
-import express from 'express';
-import multer from 'multer';
-import multerS3 from 'multer-s3';
-import path from 'path';
+import AWS from "aws-sdk";
+import express from "express";
+import multer from "multer";
+import multerS3 from "multer-s3";
+import path from "path";
+import { S3Client } from "@aws-sdk/client-s3";
 
-import Comment from '../models/comment';
-import Hashtag from '../models/hashtag';
-import Image from '../models/image';
-import Post from '../models/post';
-import User from '../models/user';
+import Comment from "../models/comment";
+import Hashtag from "../models/hashtag";
+import Image from "../models/image";
+import Post from "../models/post";
+import User from "../models/user";
 
-import { isLoggedIn } from './middleware';
+import { isLoggedIn } from "./middleware";
 
 const router = express.Router();
 
-AWS.config.update({
-  region: 'ap-northeast-2',
-  accessKeyId: process.env.S3_ACCESS_KEY_ID,
-  secretAccessKey: process.env.S3_SECRET_ACCESS_KEY,
-});
+// AWS.config.update({
+//   region: "ap-northeast-2",
+//   accessKeyId: process.env.S3_ACCESS_KEY_ID,
+//   secretAccessKey: process.env.S3_SECRET_ACCESS_KEY,
+// });
 
 const upload = multer({
   storage: multerS3({
-    s3: new AWS.S3(),
-    bucket: 'react-nodebird',
+    s3: new S3Client({
+      credentials: {
+        accessKeyId: process.env.S3_ACCESS_KEY_ID as string,
+        secretAccessKey: process.env.S3_SECRET_ACCESS_KEY as string,
+      },
+      region: "ap-northeast-2",
+    }),
+    bucket: "react-nodebird",
     key(req, file, cb) {
       cb(null, `original/${+new Date()}${path.basename(file.originalname)}`);
     },
@@ -31,7 +38,7 @@ const upload = multer({
   limits: { fileSize: 20 * 1024 * 1024 },
 });
 
-router.post('/', isLoggedIn, upload.none(), async (req, res, next) => {
+router.post("/", isLoggedIn, upload.none(), async (req, res, next) => {
   try {
     const hashtags: string[] = req.body.content.match(/#[^\s]+/g);
     const newPost = await Post.create({
@@ -39,15 +46,19 @@ router.post('/', isLoggedIn, upload.none(), async (req, res, next) => {
       UserId: req.user!.id,
     });
     if (hashtags) {
-      const promises = hashtags.map((tag) => Hashtag.findOrCreate({
-        where: { name: tag.slice(1).toLowerCase() },
-      }));
+      const promises = hashtags.map((tag) =>
+        Hashtag.findOrCreate({
+          where: { name: tag.slice(1).toLowerCase() },
+        })
+      );
       const result = await Promise.all(promises);
       await newPost.addHashtags(result.map((r) => r[0]));
     }
     if (req.body.image) {
       if (Array.isArray(req.body.image)) {
-        const promises: Promise<Image>[] = req.body.image.map((image: string) => Image.create({ src: image }));
+        const promises: Promise<Image>[] = req.body.image.map((image: string) =>
+          Image.create({ src: image })
+        );
         const images = await Promise.all(promises);
         await newPost.addImages(images);
       } else {
@@ -57,16 +68,20 @@ router.post('/', isLoggedIn, upload.none(), async (req, res, next) => {
     }
     const fullPost = await Post.findOne({
       where: { id: newPost.id },
-      include: [{
-        model: User,
-        attributes: ['id', 'nickname'],
-      }, {
-        model: Image,
-      }, {
-        model: User,
-        as: 'Likers',
-        attributes: ['id'],
-      }],
+      include: [
+        {
+          model: User,
+          attributes: ["id", "nickname"],
+        },
+        {
+          model: Image,
+        },
+        {
+          model: User,
+          as: "Likers",
+          attributes: ["id"],
+        },
+      ],
     });
     return res.json(fullPost);
   } catch (err) {
@@ -75,28 +90,32 @@ router.post('/', isLoggedIn, upload.none(), async (req, res, next) => {
   }
 });
 
-router.post('/images', upload.array('image'), (req, res) => {
+router.post("/images", upload.array("image"), (req, res) => {
   console.log(req.files);
   if (Array.isArray(req.files)) {
-    req.files.map(() => {})
+    req.files.map(() => {});
   }
   res.json((req.files as Express.MulterS3.File[]).map((v) => v.location));
 });
 
-router.get('/:id', async (req, res, next) => {
+router.get("/:id", async (req, res, next) => {
   try {
     const post = await Post.findOne({
       where: { id: req.params.id },
-      include: [{
-        model: User,
-        attributes: ['id', 'nickname'],
-      }, {
-        model: Image,
-      }, {
-        model: User,
-        as: 'Likers',
-        attributes: ['id'],
-      }],
+      include: [
+        {
+          model: User,
+          attributes: ["id", "nickname"],
+        },
+        {
+          model: Image,
+        },
+        {
+          model: User,
+          as: "Likers",
+          attributes: ["id"],
+        },
+      ],
     });
     return res.json(post);
   } catch (e) {
@@ -105,11 +124,11 @@ router.get('/:id', async (req, res, next) => {
   }
 });
 
-router.delete('/:id', isLoggedIn, async (req, res, next) => {
+router.delete("/:id", isLoggedIn, async (req, res, next) => {
   try {
     const post = await Post.findOne({ where: { id: req.params.id } });
     if (!post) {
-      return res.status(404).send('포스트가 존재하지 않습니다.');
+      return res.status(404).send("포스트가 존재하지 않습니다.");
     }
     await Post.destroy({ where: { id: req.params.id } });
     return res.send(req.params.id);
@@ -119,21 +138,23 @@ router.delete('/:id', isLoggedIn, async (req, res, next) => {
   }
 });
 
-router.get('/:id/comments', async (req, res, next) => {
+router.get("/:id/comments", async (req, res, next) => {
   try {
     const post = await Post.findOne({ where: { id: req.params.id } });
     if (!post) {
-      return res.status(404).send('포스트가 존재하지 않습니다.');
+      return res.status(404).send("포스트가 존재하지 않습니다.");
     }
     const comments = await Comment.findAll({
       where: {
         PostId: req.params.id,
       },
-      order: [['createdAt', 'ASC']],
-      include: [{
-        model: User,
-        attributes: ['id', 'nickname'],
-      }],
+      order: [["createdAt", "ASC"]],
+      include: [
+        {
+          model: User,
+          attributes: ["id", "nickname"],
+        },
+      ],
     });
     return res.json(comments);
   } catch (err) {
@@ -142,11 +163,11 @@ router.get('/:id/comments', async (req, res, next) => {
   }
 });
 
-router.post('/:id/comment', isLoggedIn, async (req, res, next) => {
+router.post("/:id/comment", isLoggedIn, async (req, res, next) => {
   try {
     const post = await Post.findOne({ where: { id: req.params.id } });
     if (!post) {
-      return res.status(404).send('포스트가 존재하지 않습니다.');
+      return res.status(404).send("포스트가 존재하지 않습니다.");
     }
     const newComment = await Comment.create({
       PostId: post.id,
@@ -158,10 +179,12 @@ router.post('/:id/comment', isLoggedIn, async (req, res, next) => {
       where: {
         id: newComment.id,
       },
-      include: [{
-        model: User,
-        attributes: ['id', 'nickname'],
-      }],
+      include: [
+        {
+          model: User,
+          attributes: ["id", "nickname"],
+        },
+      ],
     });
     return res.json(comment);
   } catch (err) {
@@ -170,11 +193,11 @@ router.post('/:id/comment', isLoggedIn, async (req, res, next) => {
   }
 });
 
-router.post('/:id/like', isLoggedIn, async (req, res, next) => {
+router.post("/:id/like", isLoggedIn, async (req, res, next) => {
   try {
     const post = await Post.findOne({ where: { id: req.params.id } });
     if (!post) {
-      return res.status(404).send('포스트가 존재하지 않습니다.');
+      return res.status(404).send("포스트가 존재하지 않습니다.");
     }
     await post.addLiker(req.user!.id);
     return res.json({ userId: req.user!.id });
@@ -184,11 +207,11 @@ router.post('/:id/like', isLoggedIn, async (req, res, next) => {
   }
 });
 
-router.delete('/:id/like', isLoggedIn, async (req, res, next) => {
+router.delete("/:id/like", isLoggedIn, async (req, res, next) => {
   try {
     const post = await Post.findOne({ where: { id: req.params.id } });
     if (!post) {
-      return res.status(404).send('포스트가 존재하지 않습니다.');
+      return res.status(404).send("포스트가 존재하지 않습니다.");
     }
     await post.removeLiker(req.user!.id);
     return res.json({ userId: req.user!.id });
@@ -198,20 +221,25 @@ router.delete('/:id/like', isLoggedIn, async (req, res, next) => {
   }
 });
 
-router.post('/:id/retweet', isLoggedIn, async (req, res, next) => {
+router.post("/:id/retweet", isLoggedIn, async (req, res, next) => {
   try {
     const post = await Post.findOne({
       where: { id: req.params.id },
-      include: [{
-        model: Post,
-        as: 'Retweet',
-      }],
+      include: [
+        {
+          model: Post,
+          as: "Retweet",
+        },
+      ],
     });
     if (!post) {
-      return res.status(404).send('포스트가 존재하지 않습니다.');
+      return res.status(404).send("포스트가 존재하지 않습니다.");
     }
-    if (req.user!.id === post.UserId || (post.Retweet && post.Retweet.UserId === req.user!.id)) {
-      return res.status(403).send('자신의 글은 리트윗할 수 없습니다.');
+    if (
+      req.user!.id === post.UserId ||
+      (post.Retweet && post.Retweet.UserId === req.user!.id)
+    ) {
+      return res.status(403).send("자신의 글은 리트윗할 수 없습니다.");
     }
     const retweetTargetId = post.RetweetId || post.id;
     const exPost = await Post.findOne({
@@ -221,28 +249,34 @@ router.post('/:id/retweet', isLoggedIn, async (req, res, next) => {
       },
     });
     if (exPost) {
-      return res.status(403).send('이미 리트윗했습니다.');
+      return res.status(403).send("이미 리트윗했습니다.");
     }
     const retweet = await Post.create({
       UserId: req.user!.id,
       RetweetId: retweetTargetId,
-      content: 'retweet',
+      content: "retweet",
     });
     const retweetWithPrevPost = await Post.findOne({
       where: { id: retweet.id },
-      include: [{
-        model: User,
-        attributes: ['id', 'nickname'],
-      }, {
-        model: Post,
-        as: 'Retweet',
-        include: [{
+      include: [
+        {
           model: User,
-          attributes: ['id', 'nickname'],
-        }, {
-          model: Image,
-        }],
-      }],
+          attributes: ["id", "nickname"],
+        },
+        {
+          model: Post,
+          as: "Retweet",
+          include: [
+            {
+              model: User,
+              attributes: ["id", "nickname"],
+            },
+            {
+              model: Image,
+            },
+          ],
+        },
+      ],
     });
     return res.json(retweetWithPrevPost);
   } catch (err) {
